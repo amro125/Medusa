d2232a8ccab501dcd5198704fe7578bd23a99d2b
Amit <amitrogel97@gmail.com>
2023-03-09 04:44:56 -0500

    Added strummer code, need to debug xarm sdk error

--- DrumStrike.py
+++ DrumStrike.py
@@ -26,13 +26,13 @@ def setup():
 
         arms[a].set_servo_angle(angle=[0, 23.1, 0, 51.4, 0, -60.8, 0], wait=False, speed=10, acceleration=0.25, is_radian=False)
 
-def spline_poly(q_i, q_f, q_in, ta, tt, ttopstop, tbotstop, pba):
+def spline_poly(q_i, q_f, q_in, ta, tt, ttopstop, tbotstop, pba, pbt, pbtopstop):
     # qi is initial pos, qf is final pos (strike), qin is new initial (return pos)
 
     # initial accel (using first half of a 5th order poly)
     # ta is double the time till max acceleration (time doing 5th order poly)
 
-    # pba is pullback amount
+    # pba is pullback amount, pbt is pullback time (.5 is good default)
 
     ########### this code calculates the trajectory for the first half (the way down) #############
 
@@ -93,7 +93,7 @@ def spline_poly(q_i, q_f, q_in, ta, tt, ttopstop, tbotstop, pba):
     # traj_top = np.ones(int(ttopstop / 0.004)) * q_i  # time stopped at top of trajectory, before strike
     # traj_top can be used for the pullback
 
-    traj_top = fifth_poly(q_i, q_i - pba, .5, 0, 0)
+    traj_top = fifth_poly(q_i, q_i - pba, pbt, pbtopstop, 0)
     thp_pb = math.floor(len(traj_top) / 2)  # halfway point of turnaround traj
     traj_top = traj_top[0:thp_pb]
 
@@ -190,16 +190,21 @@ def fifth_poly(q_i, q_f, t, ttopstop, tbotstop):
 
 
 def drumbot(traj2, traj4, traj6, arm):
+    # def drumbot(traj1, traj2, traj3, traj4, traj5, traj6, traj7, arm):
 
     #j_angles = pos
     track_time = time.time()
     initial_time = time.time()
     for i in range(min(len(traj2),len(traj4),len(traj6))):
+        # for i in range(min(len(traj1),len(traj2),len(traj3),len(traj4),len(traj5),len(traj6),len(traj7))):
         # run command
-        #start_time = time.time()
-        #j_angles[4] = traj[i]
-        #arms[numarm].set_servo_angle_j(angles=j_angles, is_radian=False)
-        jointangles = [0,traj2[i],0,traj4[i],0,traj6[i],0]
+        # start_time = time.time()
+        # j_angles[4] = traj[i]
+        # arms[numarm].set_servo_angle_j(angles=j_angles, is_radian=False)
+
+        jointangles = [0, traj2[i], 0, traj4[i], 0, traj6[i], 0]
+        # jointangles = [traj1[i], traj2[i], traj3[i], traj4[i], traj5[i], traj6[i], traj7[i]]
+
         print(traj6[i])
         arms[arm].set_servo_angle_j(angles=jointangles, is_radian=False)
         while track_time < initial_time + 0.004:
@@ -235,15 +240,17 @@ def drummer(inq,num):
 
 
     while True:
-
         # i+=1
         play = inq.get()
-        #print(i)
+        # print(i)
         print("got!")
 
         drumbot(traj2, traj4, traj6, num)
 
-        #end of run indef
+        # more joints
+        # drumbot(traj1, traj2, traj3, traj4, traj5, traj6, traj7, num)
+
+        # end of run indef
 
         # if i%3 == 1:
         #     #direction = i % 2
@@ -279,10 +286,30 @@ if __name__ == '__main__':
     speed = 0.25
     IP = [0, 23.1, 0, 51.4, 0, -60.8, 0]
     FP = [0, 51, 0, 60, 0, -12, 0]
-    #IPN = [0, 26.1, 0, 52.4, 0, -57.8, 0]
+    # IPN = [0, 26.1, 0, 52.4, 0, -57.8, 0]
     IPN = [0, 23.1, 0, 51.4, 0, -60.8, 0]
-    direction = 0 #0 is decreasing range of hit
-    #notes = np.array([64, 60, 69, 55, 62])
+    direction = 0  # 0 is decreasing range of hit
+    # notes = np.array([64, 60, 69, 55, 62])
+
+    # positions for different strike types
+    # IP1 is middle snare
+    IP1 = [0, 23.1, 0, 51.4, 0, -60.8, 0]
+    FP1 = [0.1, 48, 0.1, 60, 0.1, -12, 0.1]
+    # IP2 is pure rim
+    IP2 = [0, 23.1, 0, 51.4, 0, -60.8, 0]
+    FP2 = [0.1, 65, 0.1, 88.1, 0.1, -8, 0.1]
+    # IP3 is pure wood
+    IP3 = [0, 41.6, -16.1, 101.7, 0, 6, 0]
+    FP3 = [0.1, 60, -16.0, 81.3, 0.1, 8.2, 0.1]
+
+    # positions for lighter strikes
+    # IP6 7 and 8 are for dynamics
+    IP6 = [0, 33.1, 0, 53.4, 0, -55.8, 0]
+    FP6 = [0.1, 47, 0.1, 60, 0.1, -12, 0.1]
+    IP7 = [0, 38.1, 0, 55.4, 0, -50.8, 0]
+    FP7 = [0.1, 46, 0.1, 60, 0.1, -10, 0.1]
+    IP8 = [0, 43.1, 0, 57.4, 0, -45.8, 0]
+    FP8 = [0.1, 45, 0.1, 60, 0.1, -8, 0.1]
 
     arm0 = XArmAPI('192.168.1.236')
 
@@ -322,9 +349,87 @@ if __name__ == '__main__':
     x = 0
 
     while True:
+        # dynamics (hardest to lightest)
+
+        # traj2 = spline_poly(IP[1], FP[1]+1, IPN[1], .4, .08, 0, 0, 32)
+        # traj4 = spline_poly(IP[3], FP[3]+1, IPN[3], .3, .08, .13, .1, 0)
+        # traj6 = spline_poly(IP[5], FP[5]+1, IPN[5], .2 , .08, .35, .1, 32)
+
+        # default hit
+        # traj2 = spline_poly(IP[1], FP[1]-2, IPN[1], .5, .08, 0, 0, 24)
+        # traj4 = spline_poly(IP[3], FP[3]-2, IPN[3], .4, .08, .13, .1, 0)
+        # traj6 = spline_poly(IP[5], FP[5]-2, IPN[5], .3, .08, .35, .1, 24)
+
+        # traj2 = spline_poly(IP[1], FP[1]-4, IPN[1], .6, .08, 0, 0, 16)
+        # traj4 = spline_poly(IP[3], FP[3]-4, IPN[3], .5, .08, .13, .1, 0)
+        # traj6 = spline_poly(IP[5], FP[5]-4, IPN[5], .4, .08, .35, .1, 16)
+
+        # traj2 = spline_poly(IP[1], FP[1]-6, IPN[1], .7, .08, 0, 0, 8)
+        # traj4 = spline_poly(IP[3], FP[3]-6, IPN[3], .6, .08, .13, .1, 0)
+        # traj6 = spline_poly(IP[5], FP[5]-6, IPN[5], .5, .08, .35, .1, 8)
+
+        # ----------------------- positional hits ---------------
+
+        # default hit
+        # traj2 = spline_poly(IP[1], FP[1]-2, IPN[1], .5, .08, 0, 0, 24)
+        # traj4 = spline_poly(IP[3], FP[3]-2, IPN[3], .4, .08, .13, .1, 0)
+        # traj6 = spline_poly(IP[5], FP[5]-2, IPN[5], .3, .08, .35, .1, 24)
+
+        # pure rim
+        # traj2 = spline_poly(IP[1], FP2[1]+8, IPN[1], .5, .08, 0, 0, 24)
+        # traj4 = spline_poly(IP[3], FP2[3]+10, IPN[3], .4, .08, .13, .1, -10)
+        # traj6 = spline_poly(IP[5], FP2[5]+10, IPN[5], .3, .08, .35, .1, 24)
+
+        # pure wood (need to add in all joint in drumbot)
+        # traj1 = spline_poly(IP[0], FP3[0], IP[0], .2, .08, 0, .32, 0)
+        # traj2 = spline_poly(IP[1], FP3[1], IP[1], .5, .08, 0, 0, 16)
+        # traj3 = spline_poly(IP[2], FP3[2], IP[2], .2, .08, 0, .32, 0)
+        # traj4 = spline_poly(IP[3], FP3[3], IP[3], .4, .08, .13, 0.18, -10)
+        # traj5 = spline_poly(IP[4], FP3[4], IP[4], .2, .08, 0, 0.32, 0)
+        # traj6 = spline_poly(IP[5], FP3[5], IP[5], .3, .08, .35, 0.32, 16)
+        # traj7 = spline_poly(IP[6], FP3[6], IP[6], .2, .08, 0, 0.32, 0)
+
+        # ------------------ single, double, triple ---------------------
+
+        # default hit (single) (P 1500 D 40)
+        # traj2 = spline_poly(IP[1], FP[1]-2, IPN[1], .5, .08, 0, 0, 24, .5, 0)
+        # traj4 = spline_poly(IP[3], FP[3]-2, IPN[3], .4, .08, .13, .1, 0, .5, 0)
+        # traj6 = spline_poly(IP[5], FP[5]-2, IPN[5], .3, .08, .35, .1, 24, .5, 0)
+
+        # double (P 1000 D 40)
+        traj2 = spline_poly(IP[1], FP[1] + 3, IPN[1], .5, .08, 0, 0, 24, .5, 0)
+        traj4 = spline_poly(IP[3], FP[3], IPN[3], .4, .08, 0.1, 0, 0, .5, .13)
+        traj6 = spline_poly(IP[5], FP[5] + 3, IPN[5], .3, .08, 0.4, 0, 24, .5, .35)
+
+        # triple (P 500 D 20)
+        # traj2 = spline_poly(IP[1], FP[1]-3, IPN[1], .5, .08, 0, .2, 24, .5, 0)
+        # traj4 = spline_poly(IP[3], FP[3], IPN[3], .4, .08, 0.1, 0, 0, .5, .20)
+        # traj6 = spline_poly(IP[5], FP[5], IPN[5], .3, .08, 0.1, .1, 30, .5, .50)
+
+        # cool hit!
+        # traj2 = spline_poly(IP[1], FP[1], IPN[1], .5, .08, 0, .2, 24, .5, 0)
+        # traj4 = spline_poly(IP[3], FP[3], IPN[3], .4, .08, 0.1, 0, 0, .5, .20)
+        # traj6 = spline_poly(IP[5], FP[5], IPN[5], .3, .08, 0.1, .1, 30, .5, .50)
+
+        # traj2 = spline_poly(IP[1], FP[1], IP[1], .25, .08, .2, 0, 0)
+        # traj4 = spline_poly(IP[3], FP[3], IP[3], .25, .08, .2, 0, 0)
+        # traj6 = spline_poly(IP[5], FP[5], IP[5], .25, .08, .2, 0, 0)
+
+        # traj2 = spline_poly(IP6[1], FP6[1], IP6[1], .3, .08, .2, 0, 0)
+        # traj4 = spline_poly(IP6[3], FP6[3], IP6[3], .3, .08, .2, 0, 0)
+        # traj6 = spline_poly(IP6[5], FP6[5], IP6[5], .3, .08, .2, 0, 0)
+
+        # traj2 = spline_poly(IP7[1], FP7[1], IP7[1], .38, .08, .2, 0, 0)
+        # traj4 = spline_poly(IP7[3], FP7[3], IP7[3], .38, .08, .2, 0, 0)
+        # traj6 = spline_poly(IP7[5], FP7[5], IP7[5], .38, .08, .2, 0, 0)
+
+        # traj2 = spline_poly(IP8[1], FP8[1], IP8[1], .46, .08, .2, 0, 0)
+        # traj4 = spline_poly(IP8[3], FP8[3], IP8[3], .46, .08, .2, 0, 0)
+        # traj6 = spline_poly(IP8[5], FP8[5], IP8[5], .46, .08, .2, 0, 0)
+
         # traj2 = spline_poly(IP[1], FP[1], IPN[1], .4 + x, .08, 0, 0, 0)
         # traj4 = spline_poly(IP[3], FP[3], IPN[3], .32 + x, .08, .13, 0, 0)
-        # traj6 = spline_poly(IP[5], FP[5], IPN[5], .2 + x, .08, .35, 0, 10)
+        # traj6 = spline_poly(IP[5], FP[5], IPN[5], .2 + x, .08, .35, 0, 0)
 
         # pull back 1 (line 96, time is 0.5)
 
@@ -340,18 +445,19 @@ if __name__ == '__main__':
 
         # pull back 3
 
-        traj2 = spline_poly(IP[1], FP[1], IPN[1], .5, .08, 0, 0, 40)
-        traj4 = spline_poly(IP[3], FP[3], IPN[3], .42, .08, .13, .1, -10)
-        traj6 = spline_poly(IP[5], FP[5], IPN[5], .3, .08, .35, .1, 30)
-
+        # traj2 = spline_poly(IP[1], FP[1], IPN[1], .5, .08, 0, 0, 40)
+        # traj4 = spline_poly(IP[3], FP[3], IPN[3], .42, .08, .13, .1, -10)
+        # traj6 = spline_poly(IP[5], FP[5], IPN[5], .3, .08, .35, .1, 30)
+        #
         plt.plot(np.arange(0, len(traj2) * 0.004, 0.004), traj2, 'r', np.arange(0, len(traj4) * 0.004, 0.004), traj4,
                  'b', np.arange(0, len(traj6) * 0.004, 0.004), traj6, 'g')
         # plt.show()
+        #
+        # q0.put(1)
+        # time.sleep(3.0)
 
-        q0.put(1)
-        time.sleep(3.0)
-
-        # time.sleep(2.5 - x*30)
+        # q0.put(1)
+        # time.sleep(3.5 - x*30)
         #
         # for i in range(len(IPN)):
         #     IP[i] = IPN[i]
@@ -376,4 +482,7 @@ if __name__ == '__main__':
         #     IPN[5] = IPN[5] - 3
 
         # xArm0 = Thread(target=drummer, args=(q0, 0,))
-        #xArm0.start()
+        # xArm0.start()
+
+        q0.put(1)
+        time.sleep(3.0)
