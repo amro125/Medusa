825acd0d17bbd32e11076fb03ee86d581de09303
Amit <amitrogel97@gmail.com>
2023-03-28 19:57:28 -0400

    Updated joint plot functions, found new bug with j3-j7 data plots.

--- vision/data_plot.py
+++ vision/data_plot.py
@@ -23,33 +23,19 @@ def plot_data(processed_data):
     plt.show()
 
 
-def plot_joint_angles(processed_data):
-    algorithms = ['raw', 'smoothed']
+def plot_joint_angles(jointdata):
     joints = ['j1', 'j2', 'j3', 'j4', 'j5', 'j6', 'j7']
 
     for joint in joints:
         plt.figure(figsize=(12, 6))
         plt.title(f'{joint} joint angles over time')
 
-        for algo in algorithms:
-            plt.plot(processed_data['timestamp'], processed_data[f'{algo}_{joint}'], label=f'{algo}')
+        plt.plot(jointdata['timestamp'], jointdata[f'{joint}'], label=f'{joint}')
 
-        plt.xlabel('Timestamp')
-        plt.ylabel('Joint Angle (Degrees)')
+        plt.xlabel('Time (sec)')
+        plt.ylabel('Joint Angle (deg)')
         plt.legend()
         plt.show()
-
-
-def map_coordinates_to_angles(data):
-    angles = pd.DataFrame()
-    angles['j1'] = 0
-    angles['j2'] = 0
-    angles['j3'] = np.interp(data['shoulder_x'], [0.0, 1.0], [-90, 90])
-    angles['j4'] = np.interp(data['shoulder_y'], [0.0, 1.0], [70, 120])
-    angles['j5'] = np.interp(data['head_x'], [0.0, 1.0], [-100, 100])
-    angles['j6'] = np.interp(data['head_y'], [0.0, 1.0], [-90, 90])
-    angles['j7'] = 0
-    return angles
 
 
 def process_data(filename):
@@ -58,21 +44,6 @@ def process_data(filename):
     return data
 
 
-def process_joint_data(data: pd.DataFrame):
-    algorithms = ['raw', 'smoothed']
-
-    for algo in algorithms:
-        columns = [f'{algo}_head_x', f'{algo}_head_y', f'{algo}_head_z', f'{algo}_shoulder_x', f'{algo}_shoulder_y', f'{algo}_shoulder_z']
-        algo_data = data[columns]
-        algo_data.columns = ['head_x', 'head_y', 'head_z', 'shoulder_x', 'shoulder_y', 'shoulder_z']
-        joint_angles = map_coordinates_to_angles(algo_data)
-        for joint in ['j1', 'j2','j3', 'j4', 'j5', 'j6', 'j7']:
-            data[f'{algo}_{joint}'] = joint_angles[joint]
-
-    return data
-
-
 if __name__ == '__main__':
-    data = process_data('vision_data.csv')
-    joint_data = process_joint_data(data)
-    plot_joint_angles(joint_data)
+    data = process_data('joint_data.csv')
+    plot_joint_angles(data)
